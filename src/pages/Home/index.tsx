f00aa11ee01b872eb198306235563ec8737c2c92
JohnnyFerraz <johnny.ferraz@versatecnologia.com.br>
2023-02-15 14:10:09 -0300

    calling fake api to get the products

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
 import { CoffeeCard } from './components/CoffeeCard'
 import { IntroSection } from './components/IntroSection'
 import { CoffeeList, CoffeeListContainer, HomeContainer } from './styles'
 
+export interface Product {
+  id: string
+  name: string
+  description: string
+  image: string
+  price: number
+  availableAmount: number
+  tags: string[]
+}
+
 export function Home() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  const availableProducts = products.filter(
+    (product) => product.availableAmount > 0,
+  )
+
+  useEffect(() => {
+    api.get('/products').then((res) => setProducts(res.data))
+  }, [])
+
   return (
     <HomeContainer>
       <IntroSection />
@@ -11,17 +33,9 @@ export function Home() {
         <h1>Nossos cafés</h1>
 
         <CoffeeList>
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
+          {availableProducts.map((product) => {
+            return <CoffeeCard key={product.id} data={product} />
+          })}
         </CoffeeList>
       </CoffeeListContainer>
     </HomeContainer>
