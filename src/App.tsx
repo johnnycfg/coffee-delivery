2fadaee0cff4e898014fcca79faafa3227328761
JohnnyFerraz <johnny.ferraz@versatecnologia.com.br>
2023-02-15 14:07:51 -0300

    context for cart created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import { ThemeProvider } from 'styled-components'
+import { CartContextProvider } from './contexts/CartContext'
 import { AppRoutes } from './routes/Routes'
 import { defaultTheme } from './styles/default'
 import { GlobalStyle } from './styles/global'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <AppRoutes />
+        <CartContextProvider>
+          <AppRoutes />
+        </CartContextProvider>
       </BrowserRouter>
       <GlobalStyle />
+      <ToastContainer />
     </ThemeProvider>
   )
 }
---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,131 @@
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import { Product } from '../pages/Home'
+import { api } from '../services/api'
+
+interface CartItem {
+  id: string
+  amount: number
+}
+
+interface CartContextType {
+  cart: CartItem[]
+  addProductToCart: ({ id, amount }: CartItem) => void
+  updateProductAmountInTheCart: ({ id, amount }: CartItem) => void
+  removeProductFromCart: (id: string) => void
+}
+
+export const CartContext = createContext({} as CartContextType)
+
+interface CartContextProviderProps {
+  children: ReactNode
+}
+
+export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cart, setCart] = useState<CartItem[]>(() => {
+    const storedStateAsJSON = localStorage.getItem(
+      '@coffee-delivery:cart-state-1.0.0',
+    )
+
+    if (storedStateAsJSON) {
+      console.log(storedStateAsJSON)
+      return JSON.parse(storedStateAsJSON)
+    } else {
+      return []
+    }
+  })
+
+  async function addProductToCart({ id, amount }: CartItem) {
+    const product = await api
+      .get<Product>(`/products/${id}`)
+      .then((res) => res.data)
+
+    const newCartItem = {
+      id,
+      amount,
+    }
+
+    const productInTheCart = cart.find((cartItem) => cartItem.id === id)
+
+    if (!productInTheCart) {
+      setCart((state) => [...state, newCartItem])
+      return
+    }
+
+    if (
+      productInTheCart &&
+      productInTheCart.amount + amount > product.availableAmount
+    ) {
+      return toast(
+        'A quantidade selecionada junto a quantidade que já está no carrinho é maior que a disponível.',
+        { toastId: 'out-of-stock', position: 'top-right', type: 'error' },
+      )
+    }
+
+    const newCart = cart.map((cartItem) => {
+      if (cartItem.id === id) {
+        return { ...cartItem, amount: cartItem.amount + amount }
+      }
+
+      return cartItem
+    })
+
+    setCart(newCart)
+
+    toast.success('Produto adicionado ao carrinho.')
+  }
+
+  function removeProductFromCart(id: string) {
+    const newCart = cart.filter((cartItem) => cartItem.id !== id)
+
+    setCart(newCart)
+    toast('Produto removido do carrinho.', {
+      toastId: 'removed-from-cart',
+      position: 'top-right',
+      type: 'success',
+    })
+  }
+
+  async function updateProductAmountInTheCart({ id, amount }: CartItem) {
+    const product = await api
+      .get<Product>(`/products/${id}`)
+      .then((res) => res.data)
+
+    if (amount > product.availableAmount) {
+      return toast('A quantidade selecionada é maior que a disponível.', {
+        toastId: 'out-of-stock',
+        position: 'top-right',
+        type: 'error',
+      })
+    }
+
+    const newCart = cart.map((cartItem) => {
+      if (cartItem.id === id) {
+        return { ...cartItem, amount }
+      }
+
+      return cartItem
+    })
+
+    setCart(newCart)
+  }
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cart)
+
+    localStorage.setItem('@coffee-delivery:cart-state-1.0.0', stateJSON)
+  }, [cart])
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addProductToCart,
+        removeProductFromCart,
+        updateProductAmountInTheCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
